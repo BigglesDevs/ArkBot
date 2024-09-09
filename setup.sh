df23066e5771dfbb414c9a4aa9cdfef5d6f85fe3
BigglesDevs <bigglesdevs2003@gmail.com>
2024-09-10 00:52:13 +0100

    Added linux setup file

--- setup.sh
+++ setup.sh
@@ -9,6 +9,16 @@ source venv/bin/activate
 # Install dependencies from requirements.txt
 pip install -r requirements.txt
 
+# Create the .env file with necessary environment variables
+cat <<EOL > .env
+DISCORD_TOKEN=
+RCON_HOST=
+RCON_PORT=27020
+RCON_PASSWORD=
+OPENAI_API_KEY=
+MONGO_URI=
+EOL
+
 # Create the Start.sh file that activates the venv and runs the bot
 echo "#!/bin/bash" > Start.sh
 echo "source venv/bin/activate" >> Start.sh
